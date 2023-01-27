db70131cbf6566ddbff147c49e3a1bca956ab303
Ankit Burman <ank2911it@gmail.com>
2023-01-27 13:39:42 +0530

    Error Fixed

--- my-app/src/firebase.js
+++ my-app/src/firebase.js
@@ -1,20 +1,53 @@
+// import { initializeApp } from "firebase/app";
+//   import { getAuth } from "firebase/auth";
+//  import { getStorage } from "firebase/storage";
+//  import { getFirestore } from "firebase/firestore";
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDvVfPpVo19eLAZZig-dGUU4U4oWTg5p7M",
+//   authDomain: "chat-ab188.firebaseapp.com",
+//   projectId: "chat-ab188",
+//   storageBucket: "chat-ab188.appspot.com",
+//   messagingSenderId: "743691718979",
+//   appId: "1:743691718979:web:ca67d7412d705d3ff4dff6"
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+// export const auth = getAuth();
+// export const storage = getStorage();
+// export const db = getFirestore()
+
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDvVfPpVo19eLAZZig-dGUU4U4oWTg5p7M",
-  authDomain: "chat-ab188.firebaseapp.com",
-  projectId: "chat-ab188",
-  storageBucket: "chat-ab188.appspot.com",
-  messagingSenderId: "743691718979",
-  appId: "1:743691718979:web:ca67d7412d705d3ff4dff6"
+  apiKey: "AIzaSyBBVwF3lhF8_ASzpj-lFO41-PyhYi-0_WM",
+  authDomain: "demo1-a47a0.firebaseapp.com",
+  projectId: "demo1-a47a0",
+  storageBucket: "demo1-a47a0.appspot.com",
+  messagingSenderId: "825638739069",
+  appId: "1:825638739069:web:857490fc6a08cd38cba514",
+  measurementId: "G-VVMN19PJVH"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
 export const auth = getAuth();
 export const storage = getStorage();
-export const db = getFirestore()
+export const db = getFirestore();
--- my-app/src/pages/Register.jsx
+++ my-app/src/pages/Register.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import { createUserWithEmailAndPassword,updateProfile } from "firebase/auth";
-import { auth,storage,db } from "../firebase"
-import { useState } from 'react';
-import {  ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { doc, setDoc } from "firebase/firestore"; 
+ import { createUserWithEmailAndPassword,updateProfile } from "firebase/auth";
+ import { auth,storage,db } from "../firebase"
+ import { useState } from 'react';
+ import {  ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+ import { doc, setDoc } from "firebase/firestore"; 
 
 
 
 export default function Register(){
-    const [err, setErr]= useState(false)
+     const [err, setErr]= useState(false)
+    
     const handleSubmit = async(e)=>{
            e.preventDefault()
            const displayName = e.target[0].value;
@@ -17,56 +18,74 @@ export default function Register(){
            const file = e.target[3].files[0];
 
            
-        try{
-           const res = await createUserWithEmailAndPassword(auth, email, password)
+         try{
+            const res = await createUserWithEmailAndPassword(auth, email, password)
             
-           
+
+  
           
-           const storageRef = ref(storage, displayName);
+            const storageRef = ref(storage, displayName);
            
-           const uploadTask = uploadBytesResumable(storageRef, file);
            
-           // Register three observers:
-           // 1. 'state_changed' observer, called any time the state changes
-           // 2. Error observer, called on failure
-           // 3. Completion observer, called on successful completion
-           uploadTask.on(
+    //        // Register three observers:
+    //        // 1. 'state_changed' observer, called any time the state changes
+    //        // 2. Error observer, called on failure
+    //        // 3. Completion observer, called on successful completion
+    //   uploadTask.on(
+      
+          //     (error) => {
+          //      setErr(true);
+          //     }, 
+          //     () => {
+          //         getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
+          //              await updateProfile(res.user,{
+          //              displayName,
+          //              photoURL:downloadURL,
+          //         });
+          //         await setDoc(doc(db,"data",res.user.uid),{
+          //           uid:res.user.uid,
+          //           displayName,
+          //           email,
+          //           photoURL:downloadURL,
+          //        });
+          //     });
+          //    }
+          // );
 
 
-             (error) => {
-              setErr(true)
-             }, 
-             () => {
-               getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
-                 await updateProfile(res.user,{
-                    displayName,
-                    photoURL:downloadURL
-                 });
-                 await setDoc(doc(db,"users",res.user.uid),{
-                    uid:res.user.uid,
-                    displayName,
-                    email,
-                    photoURL:downloadURL
+          await uploadBytesResumable(storageRef, file).then(() => {
+            getDownloadURL(storageRef).then(async (downloadURL) => {
+              try {
+                await updateProfile(res.user, {
+                  displayName,
+                  photoURL: downloadURL,
                 });
-               });
+                await setDoc(doc(db, "data", res.user.uid), {
+                  uid: res.user.uid,
+                  displayName,
+                  email,
+                  photoURL: downloadURL,
+                });
+                await setDoc(doc(db, "mydb", res.user.uid), {});
+              } catch (err) {
+                console.log(err);
+                setErr(true);
+              }
+            });
+          });
+
+
+         } catch(err){
+                 setErr(true);
              }
-           );
-        
-        
-        
-        }catch(err){
-                setErr(true)
-            }
+     };
 
-
-
-    }
   return (
     <div className='formcontainer'>
         <div className="formwrapper">
             <div className="logo">Random Chat</div>
             <div className="title">Register</div>
-            <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
              <input type="text" placeholder='display name' />
              <input type="email" placeholder='email' />
              <input type="password" placeholder='Password' />
